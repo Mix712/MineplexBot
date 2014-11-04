5779699758a07c88d83bfd6247bcecf951d08d91
Dctr- <hiimdctr@gmail.com>
2014-11-04 16:08:01 -0500

    disabled songstats

--- MineplexBot.js
+++ MineplexBot.js
@@ -218,7 +218,7 @@
             website: "http://www.Mineplex.com",
             intervalMessages: [],
             messageInterval: 5,
-            songstats: true,
+            songstats: false,
             commandLiteral: "!",
             blacklists: {
                 NSFW: "https://rawgit.com/Dctr/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
